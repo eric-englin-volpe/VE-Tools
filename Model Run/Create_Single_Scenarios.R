f5d7367a8ab93c670087b78457ef3a127b070d27
sarahlohmar-volpe <sarah.lohmar@dot.gov>
2021-07-21 17:02:41 -0400

    Generalize create single scenarios

--- Model Run/Create_Single_Scenarios.R
+++ Model Run/Create_Single_Scenarios.R
@@ -1,53 +1,50 @@
 library(tidyverse)
 library(tools)
 
-#Set Up Info --
-#scenario_inputs defined by user was placed in the /models directory of Vision Eval
 
-# /models contain folder for each scenario to be run
-# inside each run folder contains levels of scenario to be run
-# inside each level folder contains files to be swapped out
+# User Input Requirements#
 
-#script expects VERSPM_NVTA Model i.e. base model to be already created
-# in order to copy from it
+## scenario_inputs_path: path where scenarios are stored i.e. B-V 
+scenario_inputs_path <- file.path("models","VERSPM_Scenarios","scenario_inputs");
+
+## base_model_path: path where base model is stored i.e. VERSPM_base_model
+base_model_path <- file.path("models", "VERSPM_Scenarios", "VERSPM_base_model")
+
+
+# Little to no changes should be required below
 
 #get the list of all scenarios to be created
-files <- list.dirs("models/scenario_inputs",full.names = FALSE,
-                   recursive = FALSE)
-
+scenario_directories <- list.dirs(scenario_inputs_path,full.names = FALSE,
+                                  recursive = FALSE)
 
 
 # iterate through all scenarios
 # Create a dataframe of scenario names
 modelNames <- vector()
 
-scenario_inputs_path <- "models/scenario_inputs/"
-
-for (item in files){
-  cat('Preparing Scenario', item, '\n\n')
-  name <- paste0(scenario_inputs_path,item)
-  models <- list.files(name, full.names = FALSE, recursive = FALSE,
-                      pattern ="[2-9]")
+for (scenario in scenario_directories){
+  cat('Preparing Scenario', scenario, '\n\n')
+  scenario_path <- file.path(scenario_inputs_path,scenario)
+  print(scenario_path)
+  single_scenario_levels <- list.files(scenario_path, full.names = FALSE, recursive = FALSE,
+                                       pattern ="[2-9]")
+  
   
   # run through each case excluding the 1 case
   # changing pattern regex above specifies what levels the script should 
   #   build folders for
   
-  for (case in models){
-    cat('\tPreparing level', case, '\t')
+  for (level in single_scenario_levels){
+    cat('\tPreparing level', level, '\t')
     
-    # create the model name i.e. VERSPM_NVTA_A2
-    base <- openModel('VERSPM_NVTA')
-    modelName <- paste0('VERSPM_NVTA_',item,case)
-    print(modelName)
-    
-    locName <- paste0(name,"/", case)
-    cat('Saving to', locName)
-    toChange <- list.files(locName,full.names = FALSE)
+    # create the model name
+    base <- openModel(base_model_path)
+    modelName <- paste0('VERSPM_',scenario,level)
+    toChange <- list.files(file.path(scenario_inputs_path,scenario,level),full.names = FALSE)
     cat('Modifying file(s):\n', paste(toChange, collapse = '\n'), '\n')
     
     #create the model run folder
-    if(!dir.exists(file.path(ve.runtime, "models", modelName))){
+    if(!dir.exists(file.path(ve.runtime, "models", "VERSPM_Scenarios", modelName))){
       runningModel <- base$copy(modelName)
     }
     
@@ -55,31 +52,34 @@ for (item in files){
     # Use ve.runtime to locate where VisionEval is installed
     
     for (f in toChange){
-      from <- file.path(ve.runtime, locName, f)
+      #fix this from line
+      from <- file.path(ve.runtime, scenario_inputs_path, scenario, level, f)
       print(from)
-      to <- file.path(ve.runtime, 'models', modelName, "inputs", f)
+      
+      #fix this to line
+      to <- file.path(ve.runtime, 'models', "VERSPM_Scenarios",modelName, "inputs", f)
       print(to)
       
-      # Check to see if the 'to' exists; some inputs are optional
-      if(file.exists(to)){
-        # verify the to and from files are different
-        stopifnot(md5sum(from) != md5sum(to))
-      }
+      # # Check to see if the 'to' exists; some inputs are optional
+      # if(file.exists(to)){
+      #   # verify the to and from files are different
+      #   stopifnot(md5sum(from) != md5sum(to))
+      # }
       
       # Save to 'msg' to suppress the printing of the result ('TRUE')
       msg <- file.copy(from, to, overwrite = TRUE)
     }
-  
     
-  modelNames <- rbind(modelNames, data.frame(name = modelName,
-                                             files = paste(toChange, collapse = ", "),
-                                             location = runningModel$modelPath,
-                                             status = runningModel$status))
+    
+    modelNames <- rbind(modelNames, data.frame(name = modelName,
+                                               files = paste(toChange, collapse = ", "),
+                                               location = runningModel$modelPath,
+                                               status = runningModel$status))
   }
   
-
-# Write records to Scenario_Status.csv file
-
-write.csv(modelNames, file.path(ve.runtime, 'models', 'Scenario_Status.csv'), row.names = F)
+  
+  # Write records to Single_Scenarios_Status.csv file
+  
+  write.csv(modelNames, file.path(ve.runtime, 'models', 'VERSPM_Scenarios','Single_Scenarios_Status.csv'), row.names = F)
 }
 
